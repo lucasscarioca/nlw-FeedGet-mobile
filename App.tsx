0be36a15d96a2447894b82c3cd0d17969588bd8d
Lucas Carioca <lucascarioca97@gmail.com>
2022-05-09 11:24:45 -0300

    NLW Stage4 finished; mobile don

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
@@ -10,14 +11,19 @@ import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium
   })
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return null;
   }
+
+  SplashScreen.hideAsync();
 
   return (
     <View style={{
